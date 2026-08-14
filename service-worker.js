68bdaa52897e4bcada88c7ccb500ce1922351d59
averylekivetz-alt <averylekivetz@icloud.com>
2026-08-14 17:29:56 -0600

    Refresh app cache for improved cover OCR

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "blush-and-books-v9";
+const CACHE_NAME = "blush-and-books-v10";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg", "./tesseract.min.js", "./tesseract-worker.min.js", "./tesseract-core-simd-lstm.wasm.js", "./tesseract-core-simd-lstm.wasm", "./eng.traineddata.gz"];
 
 self.addEventListener("install", event => {
