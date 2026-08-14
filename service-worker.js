8e8a01f5c8930a416d11da830e95713482dbf82d
averylekivetz-alt <averylekivetz@icloud.com>
2026-08-14 16:29:57 -0600

    Cache cover recognition library

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "blush-and-books-v5";
-const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg"];
+const CACHE_NAME = "blush-and-books-v6";
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg", "./tesseract.min.js"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
